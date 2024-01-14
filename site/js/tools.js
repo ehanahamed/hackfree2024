8ee64eeef0d593f6cc94e8a9bd300660dc236b0e
Ehan Ahamed <ehanahamed@ehan.dev>
2024-01-14 01:47:48 -0500

    update tools list and stuff

--- site/js/tools.js
+++ site/js/tools.js
@@ -1,7 +1,15 @@
-var currentTool = "select"
-
-function move() {
-  currentTool = "move"
+var tools = {
+  current: "select",
+  select: function () {
+    document.getElementById("toolSelect").classList.add("selected");
+    document.getElementById("toolMove").classList.remove("selected")
+    tools.current = "select";
+  },
+  move: function () {
+    document.getElementById("toolSelect").classList.remove("selected");
+    document.getElementById("toolMove").classList.add("selected")
+    tools.current = "move";
+  }
 }
 
-
+tools.select();
