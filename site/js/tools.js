811aa0f543b68831378e5cee4e336726fe40e0a2
Ehan Ahamed <ehanahamed@ehan.dev>
2024-01-14 08:08:05 -0500

    add more ui buttons

--- site/js/tools.js
+++ site/js/tools.js
@@ -2,12 +2,20 @@ var tools = {
   current: "select",
   select: function () {
     document.getElementById("toolSelect").classList.add("selected");
-    document.getElementById("toolMove").classList.remove("selected")
+    document.getElementById("toolMove").classList.remove("selected");
+    document.getElementById("toolSize").classList.remove("selected");
     tools.current = "select";
   },
   move: function () {
     document.getElementById("toolSelect").classList.remove("selected");
-    document.getElementById("toolMove").classList.add("selected")
+    document.getElementById("toolMove").classList.add("selected");
+    document.getElementById("toolSize").classList.remove("selected");
+    tools.current = "move";
+  },
+  size: function () {
+    document.getElementById("toolSelect").classList.remove("selected");
+    document.getElementById("toolMove").classList.remove("selected");
+    document.getElementById("toolSize").classList.add("selected");
     tools.current = "move";
   }
 }
